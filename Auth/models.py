0ec6a63b9678e8594011ee8f0bb69018f21fecd7
Archana-Bhamare <archusonar2@gmail.com>
2021-02-19 19:36:31 +0530

    Archana-Refactor: Add role model

--- Auth/models.py
+++ Auth/models.py
@@ -1,18 +1,22 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.timezone import now
 from .permissions import Role
 
+class Roles(models.Model):
+    role = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.role
+
 class User(AbstractUser):
     """This is the base user model which is build extending the AbstractUser models functionalities.
-    All the bassic fields like username, first_name, last_name, email, password etc are extended in this model"""
+    All the basic fields like username, first_name, last_name, email, password etc are extended in this model"""
 
-    def get_roles():
-        roles = [Role.ADMIN.value, Role.MENTOR.value, Role.STUDENT.value]
-        return ((role, role) for role in roles)      
 
     mobile = models.CharField(max_length=13)
-    role = models.CharField(choices= get_roles(), max_length=255, default="AnonymousUser")
+    role = models.ForeignKey(Roles, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.get_full_name()
@@ -23,6 +27,6 @@ class User(AbstractUser):
 
 class TokenBlackList(models.Model):
     """This model is used to store the used token so that we could check is the token is already used or not"""
-    
+
     token = models.CharField(max_length=300)
     time = models.DateTimeField(default=now)
