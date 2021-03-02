e69ab837198566325014892abef4a76b5ef5c479
malibharti5@gmail.com <malibharti5@gmail.com>
2021-03-02 16:24:31 +0530

    [Bharti]REFACTOR - Email host and port is set to enviornment

--- LMS/settings.py
+++ LMS/settings.py
@@ -133,8 +133,8 @@ MEDIA_ROOT = 'media'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_HOST_USER = os.environ.get('MAILUSER')
 EMAIL_HOST_PASSWORD = os.environ.get('MAILPASSWORD')
 
