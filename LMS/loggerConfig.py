a618437a9a69a7cf72bff7b2de4f59bf72a97f6c
Archana-Bhamare <archusonar2@gmail.com>
2021-03-03 11:39:12 +0530

    [Archana]Add: Add logs for celery

--- LMS/loggerConfig.py
+++ LMS/loggerConfig.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from celery.signals import after_setup_logger
 
 file_name = "Logs/LMS.log"
 os.makedirs(os.path.dirname(file_name), exist_ok=True)
@@ -7,3 +8,11 @@ os.makedirs(os.path.dirname(file_name), exist_ok=True)
 logging.basicConfig(filename=file_name, level=logging.INFO, filemode='w',
                     format="%(asctime)s:%(levelname)s:%(message)s")
 log = logging.getLogger()
+
+@after_setup_logger.connect
+def setup_loggers(logger, *args, **kwargs):
+    formatter = logging.Formatter('[%(asctime)s - %(name)s - %(levelname)s] - %(message)s')
+
+    log = logging.FileHandler('Logs/celery.log')
+    log.setFormatter(formatter)
+    logger.addHandler(log)
